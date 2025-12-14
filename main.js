8b20ad652e6254719e9bff6be29e52fcf3edd19c
Quang Sang <sangv6548@gmail.com>
2025-12-15 01:07:17 +0700

    feat: add sidebar

---
+++ main.js
@@ -0,0 +1,31 @@
+function loadCSS(href) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    document.head.appendChild(link);
+}
+
+async function loadComponent(id, htmlPath, cssPath = null) {
+    try {
+        
+        const response = await fetch(htmlPath);
+        if (response.ok) {
+            const htmlContent = await response.text();
+            document.getElementById(id).innerHTML = htmlContent;
+            
+        
+            if (cssPath) {
+                loadCSS(cssPath);
+            }
+        }
+    } catch (error) {
+        console.error('Lỗi:', error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    loadComponent('header-container', './components/header/header.html', './components/header/header.css');
+    loadComponent('sidebar-container', './components/sidebar/sidebar.html', './components/sidebar/sidebar.css');
+    loadComponent('body-container', './components/body/body.html', './components/content/content.css');
+});
